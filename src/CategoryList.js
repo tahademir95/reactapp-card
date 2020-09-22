257e24a985a4bbb308fd8022f4588532df185276
tahademir95 <tahadmr.1995@gmail.com>
2020-09-22 16:26:44 +0300

    State kullanımı ile Category listesinde ki category leri çoğaltma işlemi

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -3,10 +3,17 @@ import {ListGroup, ListGroupItem} from 'reactstrap'
 
 class CategoryList extends Component {
 
-    //Constructor ile props ları taşır, ama props u eklemek için bunu eklemek zorunda değilsin, eklemesende erişim olur yenireact ın versiyonları sayesinde
-    constructor(props) {
-        super(props);
-        state: {}
+    state= {
+        categories: [
+            {
+                categoryId: 1,
+                categoryName: "Beverages"
+            },
+            {
+                categoryId: 2,
+                categoryName: "Condiments"
+            }
+        ]
     }
 
     render() {
@@ -14,11 +21,11 @@ class CategoryList extends Component {
             <div>
                 <h3>{this.props.info.title}</h3>
                 <ListGroup>
-                    <ListGroupItem>Cras justo odio</ListGroupItem>
-                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                    <ListGroupItem>Morbi leo risus</ListGroupItem>
-                    <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                    {
+                        this.state.categories.map(category => (
+                            <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem>
+                        ))
+                    }
                 </ListGroup>
             </div>
         );
